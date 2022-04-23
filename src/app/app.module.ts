84947874dc95ef9d8659dae4651c18103194292d
TinoOnline <tinokurimwi@motech.dev>
2022-04-23 16:12:28 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRoutingModule
 import { AppComponent } from './app.component';
 import { SplashComponent } from './components/splash/splash.component';
 import { ListComponent } from './components/list/list.component';
@@ -25,7 +25,7 @@ import { CardComponent } from './components/card/card.component';
     HeaderComponent,
     CardComponent,
   ],
-  imports: [BrowserModule, BrowserAnimationsModule, MatSliderModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatSliderModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
