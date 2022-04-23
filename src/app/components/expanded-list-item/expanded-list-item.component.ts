e593a35b6a8af2afd7feb9a5b6d8a720984a7356
Kgotso <Kgotsokg12@gmail.com>
2022-04-23 12:41:46 +0200

    added components for all screens

---
+++ src/app/components/expanded-list-item/expanded-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expanded-list-item',
+  templateUrl: './expanded-list-item.component.html',
+  styleUrls: ['./expanded-list-item.component.css']
+})
+export class ExpandedListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
