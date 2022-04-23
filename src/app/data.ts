9411d031f4ac3992d08014a8a1c0ddacec56ed8f
TinoOnline <tinokurimwi@motech.dev>
2022-04-23 12:46:46 +0200

    adding the data

---
+++ src/app/data.ts
@@ -0,0 +1,58 @@
+export default function data() {
+  return {
+    events: [
+      {
+        name: 'The Galileo Open Air Cinema',
+        image:
+          'https://www.webtickets.co.za/admin/images/galileo/banner_S9%20Webtickets%20Image.jpg',
+        location: 'Sandringham',
+        fullLocation: '0A Rhodes Ave, Wynberg NU (2), Cape Town, 7800',
+        price: 'R109',
+        date: '23 Apr 2022',
+        time: '17:00',
+        description: `The Galileo Open Air Cinema invites you to enjoy a magical outdoor movie experience underneath a canopy of stars! This summer, The Galileo brings you two styles of cinema for your selection: Picnic & Royale.
+      
+            * The Galileo Picnic: The much-loved, classic Galileo with our local artisanal food vendors
+            * The Galileo Royale: A royal-worthy VIP experience at a selection of intimate venues around Cape Town
+            
+            Doors open at 6pm on Thursday's, Friday's and Sunday's and 5pm on Saturday's. We recommend arriving early to create special memories with your loved ones before the movie starts at sunset.`,
+      },
+      {
+        name: 'Tigresse sailing catamaran',
+        image:
+          'https://www.webtickets.co.za/admin/images/yacoobyachts/banner_Tigresse%20-%20webtickets.jpg',
+        location: 'Victoria ',
+        fullLocation: 'Victoria & Alfred Waterfront, Cape Town, 8002',
+        price: 'R220',
+        date: '23 Apr 2022',
+        time: '11:30',
+        description: `Choose from a revitalising morning boat cruise, a brisk afternoon cruise, or the beautiful Sunset cruise; this all aboard the beautiful Tigresse sailing Catamaran. Morning and afternoon cruises - 60mins in duration
+            Sunset cruise - 90mins
+            
+            Please note:
+            Tickets are only valid for the cruise indicated.
+            Please take note of your departure time.
+            Passengers missing their cruise are not entitled to refunds or rescheduling of tickets.
+            All children under the age of 12yrs are charged including babies
+            
+            For more information go to: www.tigresse.co.za`,
+      },
+      {
+        name: 'SKY-HI RIDE AT HOTEL SKY',
+        image:
+          'https://www.webtickets.co.za/admin/images/hotelsky/banner_SKY%20HI%20RIDE.jpg',
+        location: 'Hotel Sky',
+        fullLocation:
+          'Hotel Sky, CNR Lower Long & Walter Sisulu Ave, Cape Town',
+        price: 'R50 - R200',
+        date: '23 Apr 2022',
+        time: '11:00',
+        description: `If the idea of an exhilarating, gravity-defying, breath-taking drop reaching speeds of up to 100km/hr and 4G’s excites you then THIS is for you!
+      
+            Elevate your Hotel Sky experience with spectacular views and a thrilling ride. Situated as a prominent feature on the 28th floor of Hotel Sky, Sky-Hi Ride is a 35-meter thrill ride that takes you up in a chair lift, giving you a bird’s eye view of one of the most beautiful cities in the World – Cape Town. One of only three in the world, Sky High Ride extends 146 meters above the city bowl.
+            
+            Here’s a little sample of what we have in store for you: CLICK HERE!`,
+      },
+    ],
+  };
+}
